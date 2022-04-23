172234200fd9b0b4fe0b5ff87cd6acdc5e53f2cb
NicolayPages <nicolaj02@mail.ru>
2022-04-23 12:40:51 +0500

    test change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import s from "./Footer.module.scss";
 const Footer: FC = memo(() => {
   return (
     <div className={s.container}>
-      <p className={s.copy}>@Copyright Test Task for Takeoff Staff company</p>
+      <p className={s.copy}>@Copyright Test Task for Takeoff Staff company.</p>
     </div>
   );
 });
